fbec81aafbf4daf1a10f85dc1c9e4b6a04be602e
unknown <unknown@email>
2004-09-02 18:10:19 +0000

    This commit was manufactured by cvs2svn to create branch 'RELEASE_1_4_branch'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_4_branch/madman/Rpacks/marray@9859 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/maComp.R
@@ -0,0 +1,217 @@
+##########################################################################
+# maComp.R
+#
+# Simple computations for cDNA microarray objects
+#
+###########################################################################
+# Convert integer vector of indices to logical vector
+
+maNum2Logic<-function(n=length(subset), subset=TRUE)
+{
+  if(is.logical(subset))
+    return(subset)
+  if(is.numeric(subset))
+  {
+    which<-rep(FALSE,n)
+    which[subset]<-TRUE
+    return(which)
+  }
+}
+
+###########################################################################
+# Produce a table of with spot coordinates and gene names
+
+maGeneTable <- function(object)
+{
+  tmp <- data.frame(
+  GR=maGridRow(object),
+  GC=maGridCol(object),
+  SR=maSpotRow(object),
+  SC=maSpotCol(object),
+  maInfo(maGnames(object)), row.names=NULL)
+  rownames(tmp) <- rownames(maInfo(maGnames(object)))
+  colnames(tmp) <- c("Grid.R", "Grid.C", "Spot.R", "Spot.C", names(maInfo(maGnames(object))))
+  return(tmp)
+}
+
+###########################################################################
+# Generate plate IDs from dimensions of grid and spot matrices
+## Modified Feb 27, 2004
+
+maCompPlate <- function (x, n = 384)
+{
+    is.int <- function(z) trunc(z)==z
+    totalPlate <- maNspots(x)/n
+    if (is.int(totalPlate))
+      {
+        tmp <- n/(maNgr(x) * maNgc(x))
+        factor(rep(rep(1:totalPlate, rep(tmp, totalPlate)), (maNgr(x) *
+                                                             maNgc(x))))[maSub(x)]
+      }
+    else
+      {
+        totalPlate.c <- ceiling(totalPlate)
+        totalPlate.f <- floor(totalPlate)
+        tmp1 <- n/(maNgr(x) * maNgc(x))
+        tmp2 <- ((totalPlate-totalPlate.f) * n ) / (maNgr(x) * maNgc(x))
+        factor(rep(rep(1:totalPlate.c, c(rep(tmp1, totalPlate.f),tmp2)),(maNgr(x)* maNgc(x))))[maSub(x)]
+      }
+  }
+
+
+
+##maCompPlate<-function(x, n=384){
+##  totalPlate <- maNspots(x) /n
+##  tmp <- n / (maNgr(x) * maNgc(x))
+##  factor(rep(rep(1:totalPlate, rep(tmp, totalPlate)), (maNgr(x) * maNgc(x))))
+##}
+
+###########################################################################
+# Convert spot index to grid and spot matrix coordinates (4 coords)
+
+maInd2Coord <- function (x, L)
+{
+    coord<-cbind(maGridRow(L), maGridCol(L), maSpotRow(L), maSpotCol(L))[x,]
+    colnames(coord) <- c("Grid.R", "Grid.C", "Spot.R", "Spot.C")
+    coord
+}
+
+# Convert grid and spot matrix coordinates (4 coords) to spot index.
+# Works for subsetted arrays
+
+maCoord2Ind <- function (x, L)
+{
+  ngr<-maNgr(L)
+  ngc<-maNgc(L)
+  nsr<-maNsr(L)
+  nsc<-maNsc(L)
+  n<-maNspots(L)
+  ind<-(nsr * nsc)* ((x[,1] - 1) * ngc + (x[,2] - 1)) + (x[,3] - 1) *
+        nsc + x[,4]
+  ord<-order(ind)
+  ind<-ind[ord]
+  coord<-x[ord,]
+  sub<-(1:n)[maSub(L)]
+  ind<-intersect(ind, sub)
+  ind<-order(sub)[sub %in% ind]
+  ind
+}
+
+###########################################################################
+# Generate grid and spot matrix coordinates from ranges of rows and
+# columns for the grid and spot matrices
+
+maCompCoord<-function(grows, gcols, srows, scols)
+{
+  ngr <- length(grows)
+  ngc <- length(gcols)
+  nsr <- length(srows)
+  nsc <- length(scols)
+  t1 <- rep(grows, rep(nsr * nsc * ngc, ngr))
+  t2 <- rep(rep(gcols, rep(nsr * nsc, ngc)), ngr)
+  t3 <- rep(rep(srows, rep(nsc, nsr)), ngc * ngr)
+  t4 <- rep(scols, nsr * ngc * ngr)
+  coord<-cbind(t1, t2, t3, t4)
+  colnames(coord) <- c("Grid.R", "Grid.C", "Spot.R", "Spot.C")
+  coord
+}
+
+# Generate spot index from ranges of rows and columns for the grid
+# and spot matrices
+
+maCompInd<-function(grows, gcols, srows, scols, L)
+{
+  coord<-maCompCoord(grows, gcols, srows, scols)
+  maCoord2Ind(coord, L)
+}
+
+
+###########################################################################
+##
+## Added on April 5, 2004
+##
+###########################################################################
+
+maCompLayout <- function(mat, ncolumns=4)
+  {
+    if (dim(mat)[2]==3)
+      newmat <- cbind(gr=((mat[,1] - 1) %/% ncolumns) + 1, gc=((mat[,1] -1) %% ncolumns) + 1,
+                      sr=mat[,2], sc=mat[,3])
+    else
+      newmat <- mat
+
+    ngr <- max(newmat[,1]);  ngc <- max(newmat[,2])
+    nsr <- max(newmat[,3]);  nsc <- max(newmat[,4])
+    nspots <- as.integer(ngr) * as.integer(ngc) * as.integer(nsr) *   as.integer(nsc)
+
+    mlayout <- new("marrayLayout", maNgr = as.integer(ngr),
+                   maNgc = as.integer(ngc),
+                   maNsr = as.integer(nsr),
+                   maNsc = as.integer(nsc),
+                   maNspots = nspots)
+    maSub(mlayout) <- maCoord2Ind(newmat, mlayout)
+    return(mlayout)
+  }
+
+
+###########################################################################
+##
+##  Move controlCode and maGenControls from marrayTools to marrayClasses
+##  May 7, 2003
+###########################################################################
+controlCode <-
+structure(c("Buffer", "Empty", "EMPTY", "AT", "NC", "M200009348", "M200012700",
+"M200016219", "M200016205", "M200013499", "M200003425", "M200006376",
+"M200001318", "M200004477", "M200001732", "M200006590", "M200000829",
+"H200000553", "H200000680", "H200001719", "H200001847", "H200007830",
+"H200008181", "H200008484", "H200008489", "H200009216", "H200009498",
+"H200011103", "H200019704", "18S", "SSC", "mCNR", "NLG", "GABA",
+"pNICE", "M13", "Cot-1", "orward", "everse", "Genomic", "p133",
+"Blam", "T7/SP6", "ephrin", "Buffer", "Empty", "Empty", "Negative", "Negative",
+"Positive", "Positive", "Positive", "Positive", "Positive", "Positive",
+"Positive", "Positive", "Positive", "Positive", "Positive", "Positive",
+"Positive", "Positive", "Positive", "Positive", "Positive", "Positive",
+"Positive", "Positive", "Positive", "Positive", "Positive", "Positive",
+"Positive", "Buffer", "con", "con", "con", "con", "con", "Positive",
+"Negative", "Negative", "Negative", "Negative", "Negative", "con",
+"con"), .Dim = c(44, 2), .Dimnames = list(c("1", "2", "3", "4",
+"5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+"16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26",
+"27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37",
+"38", "39", "40", "41", "42", "43", "44"), c("Pattern", "Name")))
+
+## But Fix March 03, 2003
+## Replace controlCode by controlcode (1.0.6)
+## Add controlcode = controlCode (1.0.9), add defaults for gpTools and spotTools to work
+
+maGenControls <- function(Gnames, controlcode=controlCode, id="ID")
+{
+    if (is(Gnames, "marrayInfo"))
+    {
+      ifelse(is.numeric(id), tmp <- id, tmp <- grep(id, colnames(maInfo(Gnames))))
+      if(length(tmp) == 0)
+        {
+          tmp <- 1
+          print("Controls are generated from an arbitaray columns\n")
+        }
+      ID <- as.vector(as.character(maInfo(Gnames[,tmp])[[1]]))
+    }
+  else
+    {
+      if(is.null(dim(Gnames)))
+        ID <- as.character(Gnames)
+      else
+        {
+          ifelse(is.numeric(id), tmp <- id, tmp <- grep(id, colnames(Gnames)))
+          ID <- as.vector(as.character(Gnames[,tmp]))
+        }
+    }
+  Control <- rep("probes", length(ID))
+  for(i in 1:nrow(controlcode))
+    {
+      position <- grep(as.vector(controlcode[i,"Pattern"]), ID)
+      if(length(position) > 0)
+        Control[position] <- as.vector(controlcode[i, "Name"])
+    }
+  return(Control)
+}
---
+++ R/maNorm.R
@@ -0,0 +1,386 @@
+###########################################################################
+# maNorm.R
+#
+# Functions for location and scale normalization for two-color cDNA microarrays
+#
+###########################################################################
+
+############################################################################
+# maNormMain
+# Main within-slide location and scale normalization function
+
+maNormMain<-function(mbatch,
+                     f.loc=list(maNormLoess()),
+                     f.scale=NULL,
+                     a.loc=maCompNormEq(),
+                     a.scale=maCompNormEq(),
+                     Mloc=TRUE, Mscale=TRUE, echo=FALSE)
+{
+
+    if (is(mbatch, "marrayRaw"))
+    {
+        mnorm<-as(mbatch, "marrayNorm")
+        M<-Ml<-Ms<-NULL
+    }
+    if (is(mbatch, "marrayNorm"))
+        mnorm<-mbatch
+
+  slot(mnorm, "maNormCall") <- match.call()
+
+  if(length(f.loc)>0)
+    M<-Ml<-NULL
+  if(length(f.scale)>0)
+    M<-Ms<-NULL
+
+  for(i in 1:ncol(maM(mbatch)))
+  {
+
+    if(echo)
+      cat(paste("Normalizing array ", i, ".\n", sep=""))
+    m<-mbatch[,i]
+    M1<-M2<-NULL
+    Mnorm<-maM(m)
+
+    # Location
+    if(length(f.loc)>0)
+    {
+      for(func in f.loc)
+        M1<-cbind(M1, func(m))
+
+      if(length(f.loc)>1)
+          M1 <- rowSums(M1*a.loc(maA(m),length(f.loc)))
+      Ml<-cbind(Ml,M1)
+      Mnorm<-(Mnorm - M1)
+    }
+
+
+    # Scale
+    if(length(f.scale)>0)
+    {
+      # Scale computed for location normalized data
+      m<-mnorm[,i]
+      slot(m, "maM")<-Mnorm
+      for(func in f.scale)
+        M2<-cbind(M2, func(m))
+
+      if(length(f.scale)>1)
+          M2 <- rowSums(M2*a.scale(maA(m),length(f.scale)))
+      Ms<-cbind(Ms,M2)
+      Mnorm<-Mnorm/M2
+    }
+
+    M<-cbind(M, Mnorm)
+  }
+
+  slot(mnorm, "maM")<-M
+  if(length(f.loc)>0 & Mloc)
+    slot(mnorm, "maMloc")<-Ml
+  if(length(f.scale)>0 & Mscale)
+    slot(mnorm, "maMscale")<-Ms
+
+  return(mnorm)
+}
+
+############################################################################
+# maNorm
+# Wrapper function around maNormMain for simple normalization procedures
+#
+# From Gordon's Mar 17 E-mail
+# loess(y~x,span=0.4,degree=1,family="symmetric",control=loess.control(trace.hat="ap
+# proximate",iterations=5,surface="direct"))
+#
+
+maNorm<-function(mbatch,
+	norm=c("printTipLoess", "none", "median", "loess", "twoD", "scalePrintTipMAD"),
+	subset=TRUE, span=0.4, Mloc=TRUE, Mscale=TRUE, echo=FALSE, ...)
+{
+  ## Set normalization defaults
+  opt <- list(...)
+
+  ## norm <- unlist(strsplit(norm, split=""))[1]
+  norm.method <- match.arg(norm)
+  if(echo)
+    cat(paste("Normalization method: ", norm.method, ".\n", sep=""))
+
+  switch(norm.method,
+         none = maNormMain(mbatch, f.loc=NULL,
+           Mloc=Mloc, Mscale=Mscale, echo=echo),
+
+         median = maNormMain(mbatch,
+           f.loc=list(maNormMed(x=NULL, y="maM", subset=subset)),
+           Mloc=Mloc, Mscale=Mscale, echo=echo),
+
+         loess = maNormMain(mbatch,
+           f.loc=list(maNormLoess(x="maA", y="maM", z=NULL, w=NULL,
+             subset=subset, span=span, ...)),
+           Mloc=Mloc, Mscale=Mscale, echo=echo),
+
+         twoD = maNormMain(mbatch,
+           f.loc=list(maNorm2D(x = "maSpotRow", y = "maSpotCol",
+             z = "maM", g = "maPrintTip",
+             w = NULL, subset = subset, span = span, ...)),
+           Mloc=Mloc, Mscale=Mscale, echo=echo),
+
+         printTipLoess = maNormMain(mbatch,
+           f.loc=list(maNormLoess(x="maA", y="maM", z="maPrintTip",
+             w=NULL, subset=subset, span=span, ...)),
+           Mloc=Mloc, Mscale=Mscale, echo=echo),
+
+         scalePrintTipMAD = maNormMain(mbatch,
+           f.loc=list(maNormLoess(x="maA", y="maM", z="maPrintTip",
+             w=NULL, subset=subset, span=span, ...)),
+           f.scale=list(maNormMAD(x="maPrintTip", y="maM", geo=TRUE,
+             subset=subset)),
+           Mloc=Mloc, Mscale=Mscale, echo=echo))
+}
+
+############################################################################
+# maNormScale
+# Wrapper function around maNormMain for simple scale normalization procedures
+
+maNormScale<-function(mbatch,
+		norm=c("globalMAD", "printTipMAD"),
+		subset=TRUE, geo=TRUE, Mscale=TRUE, echo=FALSE)
+{
+  ## norm <- unlist(strsplit(norm, split=""))[1]
+
+  norm.method <- match.arg(norm)
+  if(echo)
+    cat(paste("Normalization method: ", norm.method, ".\n", sep=""))
+
+  if(norm.method == "globalMAD")
+  {
+     # Not a very nice way to do between slide norm
+     if(!geo)
+       mnorm<-maNormMain(mbatch,
+          f.loc=NULL,
+	  f.scale=list(maNormMAD(x=NULL, y="maM", geo=geo, subset=subset)),
+	  Mscale=Mscale, echo=echo)
+     if(geo)
+     {
+       mnorm<-maNormMain(mbatch,
+         f.loc=NULL,
+	 f.scale=list(maNormMAD(x=NULL, y="maM", geo=FALSE, subset=subset)),
+	 Mscale=TRUE, echo=echo)
+         #mgeo<-apply(maMscale(mnorm), 1, function(z) exp(mean(log(z))))
+         mgeo<- exp(mean(log(maMscale(mnorm)[1,])))
+
+       maM(mnorm)<-maM(mnorm)*mgeo
+       if(Mscale)
+         maMscale(mnorm)<-maMscale(mnorm)/mgeo
+       if(!Mscale)
+         maMscale(mnorm)<-NULL
+     }
+  }
+
+  if(norm.method == "printTipMAD")
+     mnorm<-maNormMain(mbatch,
+        f.loc=NULL,
+	f.scale=list(maNormMAD(x="maPrintTip", y="maM", geo=geo,
+		subset=subset)),
+	Mscale=Mscale, echo=echo)
+
+  return(mnorm)
+}
+
+###########################################################################
+# Median location normalization
+###########################################################################
+# maMed
+# General function: median of y within values of x
+
+maMed<-function(x, y, subset=TRUE)
+{
+  subset<-maNum2Logic(length(y), subset)
+  yfit<-rep(NA,length(y))
+  for(i in unique(x))
+    yfit[x==i]<-median(y[(x==i)&subset], na.rm=TRUE)
+  return(yfit)
+}
+
+#########################
+# maNormMed
+# Function for objects of class marrayRaw
+
+maNormMed<-function(x=NULL, y="maM", subset=TRUE)
+{
+  function(m)
+  {
+    if(is.character(x))
+      maMed(x=eval(call(x,m)), y=eval(call(y,m)), subset=subset)
+    else
+      maMed(x=TRUE, y=eval(call(y,m)), subset=subset)
+  }
+}
+
+###########################################################################
+# Loess location normalization
+###########################################################################
+# maLoess
+# General function: regress y on x within values of z
+# In our case, x=NA iff y=NA
+## Jean modify Nov 5th, April 9, 2003
+## Very ugly fix
+
+maLoess <-
+function (x, y, z, w = NULL, subset = TRUE, span = 0.4, ...)
+{
+  opt <- list(...)
+  subset <- maNum2Logic(length(y), subset)
+  yfit <- rep(NA, length(y))
+  good <- !(is.infinite(x) | is.infinite(y) | is.na(x) | is.na(y))
+  if(length(z) == 1) info <- z
+  else
+    info <- z[subset]
+  for (i in unique(info))
+    {
+      which <- z == i
+      defs <- list(degree=1,
+                   family="symmetric",
+                   control=loess.control(trace.hat="approximate",iterations=5, surface="direct"),
+                   subset = which & subset & good,
+                   span=span,
+                   na.action = na.omit,
+                   weights=w)
+      args <- maDotsMatch(c(defs, opt), formals(args("loess")))
+      fit <- loess(y ~ x,  weights = args$w, subset = args$subset,
+                   span = args$span, na.action = args$na.action,
+                   degree = args$degree, family=args$family,
+                   control=args$control)
+##      gc(TRUE)
+      ##      fit <- loess(y ~ x, weights = w, subset = which & subset &
+      ##                   good, span = span, na.action = na.omit)
+      yfit[which & good] <- predict(fit, x[which & good])
+    }
+    return(yfit)
+}
+
+
+#########################
+# maNormLoess
+# Function for objects of class marrayRaw
+
+maNormLoess<-function(x="maA", y="maM", z="maPrintTip", w=NULL, subset=TRUE, span=0.4, ...)
+{
+  function(m)
+  {
+    if(is.character(z))
+      maLoess(x=eval(call(x,m)), y=eval(call(y,m)), z=eval(call(z,m)), w=w, subset=subset, span=span, ...)
+    else
+      maLoess(x=eval(call(x,m)), y=eval(call(y,m)), z=TRUE, w=w, subset=subset, span=span, ...)
+  }
+}
+
+###########################################################################
+# 2D spatial location normalization
+###########################################################################
+# ma2D
+# General function: regress z on x*y within values of g
+
+ma2D<-function(x, y, z, g, w=NULL, subset=TRUE, span=0.4, ...)
+{
+  opt <- list(...)
+  subset<-maNum2Logic(length(y), subset)
+  zfit<-rep(NA,length(z))
+  # Deal with NA, NaN, Inf
+  good <- !(is.infinite(z) | is.na(z))
+
+  for(i in unique(g))
+  {
+    which<-g==i
+    defs <- list(degree=1,
+                 family="symmetric",
+                 control=loess.control(trace.hat="approximate",iterations=5, surface="direct"),
+                 subset = which & subset & good,
+                 span=span,
+                 na.action = na.omit,
+                 weights=w)
+    args <- maDotsMatch(c(defs, opt), formals(args("loess")))
+    fit <- loess(z ~ x * y,  weights = args$w, subset = args$subset,
+                 span = args$span, na.action = args$na.action,
+                 degree = args$degree, family=args$family,
+                 control=args$control)
+    ##    gc(TRUE)
+    ##    fit<-loess(z ~ x * y, weights=w, subset=subset&which&good, span=span, na.action=na.omit, ...)
+    zfit[which]<-predict(fit, data.frame(x=x[which],y=y[which]))
+   }
+   return(zfit)
+}
+
+#########################
+# maNorm2D
+# Function for objects of class marrayRaw
+
+maNorm2D<-function(x="maSpotRow", y="maSpotCol", z="maM", g="maPrintTip", w=NULL, subset=TRUE, span=0.4, ...)
+{
+  function(m)
+    ma2D(x=eval(call(x,m)), y=eval(call(y,m)), z=eval(call(z,m)), g=eval(call(g,m)), w=w, subset=subset, span=span, ...)
+}
+
+###########################################################################
+# MAD scale normalization
+###########################################################################
+# maMAD
+# General function: MAD of y within values of x
+
+maMAD<-function(x, y, geo=TRUE, subset=TRUE)
+{
+  subset<-maNum2Logic(length(y), subset)
+  yfit<-rep(NA,length(y))
+  m<-rep(NA,length(unique(x)))
+  for(i in unique(x))
+  {
+    m[i]<-mad(y[(x==i)&subset], na.rm=TRUE)
+    yfit[x==i]<-m[i]
+  }
+  if(geo)
+   yfit<-yfit/exp(mean(log(m)))
+  return(yfit)
+}
+
+#########################
+# maNormMAD
+# Function for objects of class marrayRaw
+
+maNormMAD<-function(x=NULL, y="maM", geo=TRUE, subset=TRUE)
+{
+  function(m)
+  {
+    if(is.character(x))
+      maMAD(x=eval(call(x,m)), y=eval(call(y,m)), geo=geo, subset=subset)
+    else
+      maMAD(x=TRUE, y=eval(call(y,m)), geo=geo, subset=subset)
+  }
+}
+
+###########################################################################
+# Weights for composite normalization
+###########################################################################
+# maCompNormEq
+# Equal weights for composite normalization
+
+maCompNormEq<-function()
+{
+  function(x,n)
+  {
+    return(matrix(1/n,length(x),n))
+  }
+}
+
+#########################
+# maCompNormA
+# Weights for composite intensity dependent normalization
+
+maCompNormA<-function()
+{
+  function(x,n)
+  {
+    if(n!=2)
+      stop("Can only do composite location normalization for 2 functions in f.loc")
+    f<-ecdf(x)
+    a<-f(x)
+    return(as.matrix(cbind(a,1-a)))
+  }
+}
+
+###########################################################################
